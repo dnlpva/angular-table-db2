1be6e00d34a11bf92f2e28caffadb76e913ff577
Danilo Paiva <danilop@hatley.com>
2021-09-28 11:25:09 -0400

    Project rename

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'intranet-office'`, () => {
+  it(`should have as title 'angular-table-db1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('intranet-office');
+    expect(app.title).toEqual('angular-table-db1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('intranet-office app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-table-db1 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'intranet-office';
+  title = 'angular-table-db1';
 
   constructor() { }
 
